0a4a74cb8ca2bb4d41fe2341d165f8ac6fe22799
Trey Walker <kasu@Treys-Air.home.arpa>
2023-05-03 22:59:29 -0500

    restructure files

---
+++ cloud.js
@@ -0,0 +1,9 @@
+const body = document.body;
+document.body.remove();
+console.log('test');
+console.log(body);
+
+document.HTML.appendChild(body);
+
+// const wordNodes = document.querySelectorAll('p');
+// wordNodes.forEach((e) => e.remove());
--- popup/popup.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const header = document.createElement('h1');
-  header.innerText = 'a!!';
-  document.querySelector('body').appendChild(header);
-  const wordNodes = document.querySelectorAll('p');
-  wordNodes.forEach((e) => e.remove());
-});
